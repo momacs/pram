44fd4fefc599a82ff1c69693f5b359969bed0b0e
Tommy Loboda <tomek.loboda@gmail.com>
2019-08-27 17:31:08 -0400

    Commented out secondary code

--- src/sim/00-tests/trajectory.py
+++ src/sim/00-tests/trajectory.py
@@ -5,14 +5,16 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 import numpy as np
 
 from pram.entity import Group
-from pram.rule   import FibonacciSeq
+from pram.rule   import SIRSModel
 from pram.sim    import Simulation
 from pram.traj   import Trajectory, TrajectoryEnsemble
-from pram.rule   import SIRSModel
 
 
 # ----------------------------------------------------------------------------------------------------------------------
 # (0) Init:
+
+def get_out_dir(filename):
+    return os.path.join(os.path.dirname(__file__), 'mass-dynamics', 'out', filename)
 
 fpath_db = os.path.join(os.path.dirname(__file__), f'trajectory.sqlite3')
 
@@ -20,36 +22,39 @@ fpath_db = os.path.join(os.path.dirname(__file__), f'trajectory.sqlite3')
 # ----------------------------------------------------------------------------------------------------------------------
 # (1) Create the database and run a trajectory ensemble:
 
-# if os.path.isfile(fpath_db):
-#     os.remove(fpath_db)
-#
-# te = (
-#     TrajectoryEnsemble(fpath_db).
-#         add_trajectories([
-#             Trajectory(
-#                 f'SIR: b={round(beta,2)}',
-#                 None,
-#                 (
-#                     Simulation().
-#                     add([
-#                         SIRSModel('flu', beta, 0.50, 0.00),
-#                         Group(m=1000, attr={ 'flu': 's' })
-#                     ])
-#                 )
-#             ) for beta in np.arange(0.01, 0.03, 0.01)
-#         ]).
-#         run(10)
-# )
+if os.path.isfile(fpath_db):
+    os.remove(fpath_db)
+
+te = (TrajectoryEnsemble(fpath_db).
+    add_trajectories([
+        Trajectory(f'SIR: b={round(beta,2)}', None,
+            (Simulation().
+                add([
+                    SIRSModel('flu', beta, 0.50, 0.00),
+                    Group(m=1000, attr={ 'flu': 's' })
+                ])
+            )
+        ) for beta in [0.05]  # np.arange(0.05, 0.06, 0.01)
+    ]).
+    set_group_names([
+        (0, 'S', Group.gen_hash(attr={ 'flu': 's' })),
+        (1, 'I', Group.gen_hash(attr={ 'flu': 'i' })),
+        (2, 'R', Group.gen_hash(attr={ 'flu': 'r' }))
+    ]).
+    run(100)
+)
 
 
 # ----------------------------------------------------------------------------------------------------------------------
-# (2) Restore a trajectory ensemble from the database:
+# (2) Restore a trajectory ensemble from the database for additional runs:
 
-te = (
-    TrajectoryEnsemble(fpath_db).
-    run(5)
-)
+# TrajectoryEnsemble(fpath_db).run(20)
 
-# for t in te.traj.values():
-#     print(t.id)
-#     t.plot_states((1600,900), os.path.join(os.path.dirname(__file__), 'mass-dynamics', 'states.png'))
+# ----------------------------------------------------------------------------------------------------------------------
+# (3) Restore a trajectory ensemble from the database for analysis:
+
+# te = TrajectoryEnsemble(fpath_db)
+
+# te.traj[1].plot_mass_flow_time_series(filepath=get_out_dir('states.png'), iter_range=(-1,10), v_prop=False, e_prop=True)
+# te.traj[1].plot_group_mass_streamgraph((900,600), get_out_dir('group-streamgraph.png'), (-1,50))
+# te.traj[1].plot_heatmap((800,800), get_out_dir('heatmap.png'), (-1,20))
