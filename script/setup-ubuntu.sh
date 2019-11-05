689184065a83d1107b9162158540bdc616f78c8d
Tommy Loboda <tomek.loboda@gmail.com>
2019-11-05 06:10:06 -0500

    Updated the script

--- script/setup-ubuntu.sh
+++ script/setup-ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-do_venv=1
+do_venv=1  # set to 0 to install without a venv
 name=pram
 
 if [ -d $name ]; then
@@ -8,13 +8,22 @@ if [ -d $name ]; then
     exit 1
 fi
 
+# Ask for intent confirmation:
+if [ "$do_venv" -eq "1" ]; then
+    read -p 'Install with Python venv? [y/n] ' res
+    if [ "$res" -ne "y" -o "$res" -ne "Y" ]; then exit 0; fi
+else
+    read -p 'Install without Python venv (i.e., into system site directories)? [y/n] ' res
+    if [ "$res" -ne "y" -o "$res" -ne "Y" ]; then exit 0; fi
+fi
+
 # Update the OS:
-sudo apt update
-sudo apt upgrade
+sudo apt -y update
+sudo apt -y upgrade
 
 # Install required packages:
 sudo apt -y install git gcc build-essential autoconf libtool pkg-config libgmp-dev libcairomm-1.0-dev libffi-dev libsparsehash-dev
-sudo apt -y install python3-dev python3.6-dev python3-numpy python3-pip python3-cairo python3-cairo-dev python3-cairocffi
+sudo apt -y install python3-dev python3.6-dev python3-pip python3-venv python3-numpy python3-cairo python3-cairo-dev python3-cairocffi
 sudo apt -y install firefox firefox-geckodriver  # for Altair
 sudo apt -y install aptitude  # for GCAL
 sudo aptitude -y install libcgal-dev
@@ -32,17 +41,17 @@ sudo apt update
 sudo apt -y install python-graph-tool python3-graph-tool
 
 # Clone the repo, install Python dependencies, and run a simple PRAM simulation:
-if [ "$do_venv" == "1" ]; then
-	python3 -m venv $name
-	cd $name
-	source ./bin/activate
-	
-	git init
-	git remote add origin https://github.com/momacs/$name
-	git pull origin master
+if [ "$do_venv" -eq "1" ]; then
+    python3 -m venv $name
+    cd $name
+    source ./bin/activate
+    
+    git init
+    git remote add origin https://github.com/momacs/$name
+    git pull origin master
 else
-	git clone https://github.com/momacs/$name
-	cd $name
+    git clone https://github.com/momacs/$name
+    cd $name
 fi
 
 pip3 install -r requirements.txt
