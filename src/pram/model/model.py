87ebce7b78f05e21af962ec8e131ef2668ebba27
ubiq-x <tomek.loboda@gmail.com>
2020-04-07 02:06:33 -0400

    Added __all__

--- src/pram/model/model.py
+++ src/pram/model/model.py
@@ -1,5 +1,7 @@
 from abc import abstractmethod, ABC
 
+__all__ = ['ModelConstructionError', 'Model', 'Solver', 'MCSolver', 'ODESolver']
+
 
 # ----------------------------------------------------------------------------------------------------------------------
 class ModelConstructionError(Exception): pass
